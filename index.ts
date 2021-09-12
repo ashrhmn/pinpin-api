9018549188c60a7968643c10c9a106ccd08eb187
Ashik Rahman <ashrhmn99@gmail.com>
2021-09-12 14:16:06 +0600

    Add pg db

--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import client from './database'
 
 const app = express()
 
+app.get('/tables', (req: Request, res: Response) => {
+    client.connect()
+    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+            console.log(JSON.stringify(row));
+        }
+        client.end();
+    });
+})
+
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`)
-)
+app.listen(() => console.log(`http://localhost:${PORT}`))
