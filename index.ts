2d81876c799fc71834ea7b65ffa9da1dbef57db1
Ashik Rahman <ashrhmn99@gmail.com>
2021-09-12 15:59:04 +0600

    Add SSL Config

--- index.ts
+++ index.ts
@@ -1,41 +1,46 @@
-import express, { Request, Response } from 'express'
-import { createConnection, getRepository } from 'typeorm'
+import express, { Request, Response } from "express";
+import { createConnection, getRepository } from "typeorm";
 
-import User from './model/User'
+import User from "./model/User";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/users', async(req: Request, res: Response) => {
-    const result = await getRepository(User).find()
-    return res.json(result)
-})
+app.get("/users", async (req: Request, res: Response) => {
+  const result = await getRepository(User).find();
+  return res.json(result);
+});
 
-app.post('/users',async(req:Request, res:Response)=>{
-    const username = req.body.username
-    const password = req.body.password
-    const role = req.body.role
+app.post("/users", async (req: Request, res: Response) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const role = req.body.role;
 
-//    const newData = await getRepository(User).create({username,password,role})
+  //    const newData = await getRepository(User).create({username,password,role})
 
-    const result = await getRepository(User).create({username,password,role}).save()
-    return res.json(result)
-})
+  const result = await getRepository(User)
+    .create({ username, password, role })
+    .save();
+  return res.json(result);
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 createConnection({
-    type:'postgres',
-    url: process.env.DATABASE_URL || "postgresql://ash:@localhost:5432/pinpin",
-    synchronize : true,
-    logging:true,
-    entities:[User]
+  type: "postgres",
+  url: process.env.DATABASE_URL || "postgresql://ash:@localhost:5432/pinpin",
+  synchronize: true,
+  logging: true,
+  entities: [User],
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
-.then(connection=>{
+  .then((connection) => {
     // console.log(connection);
-    app.listen(PORT,() => console.log(`http://localhost:${PORT}`))    
-})
-.catch(error=>{
+    app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+  })
+  .catch((error) => {
     console.log(error);
-})
+  });
