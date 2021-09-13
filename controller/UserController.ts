92d2440d74e0ff3a119409a0fac61914cc355b52
Ashik Rahman <ashrhmn99@gmail.com>
2021-09-13 13:09:20 +0600

    Add all controller and routes fro user

--- controller/UserController.ts
+++ controller/UserController.ts
@@ -5,9 +5,9 @@ import User from "../model/User";
 export const getAllUser = async (req: Request, res: Response) => {
   try {
     const result = await getRepository(User).find();
-    return res.json(result);
+    return res.status(200).json(result);
   } catch (error) {
-    return res.json(error);
+    return res.status(500).json(error);
   }
 };
 
@@ -17,8 +17,56 @@ export const saveNewUser = async (req: Request, res: Response) => {
     const result = await getRepository(User)
       .create({ username, password, role })
       .save();
-    return res.json(result);
+    return res.status(201).json(result);
   } catch (error) {
-    return res.json(error);
+    return res.status(500).json(error);
+  }
+};
+
+export const getUser = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const result = await getRepository(User).findOne(id);
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const dataExists = await getRepository(User).findOne(id);
+    if (dataExists) {
+      const result = await getRepository(User).delete(id);
+      return res.status(200).json(result);
+    } else {
+      return res.status(422).json({ msg: `Invalid id parameter, got ${id}` });
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const id = parseInt(req.params.id);
+    const { username, password, role } = req.body;
+    const dataExists = await getRepository(User).findOne(id);
+    if (dataExists) {
+      const result = await getRepository(User)
+        .create({
+          id,
+          username: username || dataExists.username,
+          password: password || dataExists.password,
+          role: role || dataExists.role,
+        })
+        .save();
+      return res.status(201).json(result);
+    } else {
+      return res.status(422).json({ msg: `Invalid id parameter, got ${id}` });
+    }
+  } catch (error) {
+    return res.status(500).json(error);
   }
 };
--- routes/UserRoutes.ts
+++ routes/UserRoutes.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
-import { getAllUser, saveNewUser } from "../controller/UserController";
+import {
+  deleteUser,
+  getAllUser,
+  getUser,
+  saveNewUser,
+  updateUser,
+} from "../controller/UserController";
 
 const router = Router();
 
 router.get("/", getAllUser);
 
+router.get("/:id", getUser);
+
 router.post("/", saveNewUser);
+
+router.put("/:id", updateUser);
+
+router.delete("/:id", deleteUser);
 
 export default router;
