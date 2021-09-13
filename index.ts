96a110093d56106d8b47bd255ebeb463e14fa55b
Ashik Rahman <ashrhmn99@gmail.com>
2021-09-13 10:08:09 +0600

    Add env_type var

--- index.ts
+++ index.ts
@@ -6,32 +6,35 @@ import User from "./model/User";
 import PinDataRoutes from "./routes/PinDataRoutes";
 import UserRoutes from "./routes/UserRoutes";
 
+const environment = process.env.ENV_TYPE || "DEV";
+
 const app = express();
 
 app.use(express.json());
 
 //routes
-app.use('/users',UserRoutes)
-app.use('/pinData',PinDataRoutes)
+app.use("/users", UserRoutes);
+app.use("/pinData", PinDataRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 createConnection({
   type: "postgres",
-  url: process.env.DATABASE_URL || "postgresql://ash:@localhost:5432/pinpin",
+  url:
+    environment != "DEV"
+      ? process.env.DATABASE_URL
+      : "postgresql://ash:@localhost:5432/pinpin",
   synchronize: true,
   logging: true,
   entities: [User, PinData],
-  ssl: process.env.PORT
-    ? {
-        rejectUnauthorized: false,
-      }
-    : null,
+  ssl:
+    environment != "DEV"
+      ? {
+          rejectUnauthorized: false,
+        }
+      : null,
 })
-  .then((connection) => {
-    // console.log(connection);
-    console.log(process.env);
-    
+  .then(() => {
     app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
   })
   .catch((error) => {
