e64ca68356d48033b3f0d9fc5912fd16e77a018d
Ashik Rahman <ashrhmn99@gmail.com>
2021-09-20 08:24:55 +0600

    Add info msg in authcontroller

--- controller/AuthController.ts
+++ controller/AuthController.ts
@@ -8,16 +8,18 @@ export const loginUser = async (req: Request, res: Response) => {
   try {
     const { username, password } = req.body;
     const user = await getRepository(User).findOne({ username });
-    if (!user) return res.json({ msg: "User not found" });
+    if (!user) return res.json({ msg: "User not found", token: null });
     const isCorrectPassword = await bcryptjs.compare(password, user.password);
     if (!isCorrectPassword)
-      return res.json({ msg: "Invalid Password" });
+      return res.json({ msg: "Invalid Password", token: null });
 
     const token = jwt.sign(
       { username: user.username, role: user.role },
       process.env.SECRET || "sshhh"
     );
-    return res.header("authToken", token).json({ token });
+    return res
+      .header("authToken", token)
+      .json({ token, msg: "Logged In Successfully" });
   } catch (error) {
     return res.status(500).json(error);
   }
@@ -36,7 +38,9 @@ export const signUpUser = async (req: Request, res: Response) => {
         role,
       })
       .save();
-    return res.status(201).json({msg:"Signup Successfull, Login to continue"});
+    return res
+      .status(201)
+      .json({ msg: "Signup Successfull, Login to continue" });
   } catch (error) {
     return res.status(500).json(error);
   }
@@ -45,10 +49,14 @@ export const signUpUser = async (req: Request, res: Response) => {
 export const currentUser = async (req: Request, res: Response) => {
   try {
     const token = req.header("authToken");
-    if (!token) return res.json({ msg: "Not logged in" });
+    if (!token)
+      return res.json({ msg: "Not logged in", isLoggedIn: false, user: null });
     const verifiedUser = jwt.verify(token, process.env.SECRET || "sshhh");
-    if (!verifiedUser) return res.json({ msg: "Access Denied" });
-    return res.status(200).json(verifiedUser);
+    if (!verifiedUser)
+      return res.json({ msg: "Access Denied", isLoggedIn: false, user: null });
+    return res
+      .status(200)
+      .json({ msg: "", isLoggedIn: true, user: verifiedUser });
   } catch (error) {
     return res.status(401).json(error);
   }
